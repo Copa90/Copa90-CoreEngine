2140a6e4b00f4df71939010782225a427fa555ab
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-07-18 12:43:28 +0430

    Make Prediction Ending Schedular & Add Win Points to Champions and Total

--- common/models/predict.js
+++ common/models/predict.js
@@ -1,3 +1,4 @@
+var cron = require('cron')
 var app = require('../../server/server')
 
 var utility	= require('../../public/utility')
@@ -5,6 +6,99 @@ var statusConfig = require('../../config/predictStatus.json')
 var estimateStatusConfig = require('../../config/estimateStatus.json')
 
 module.exports = function(predict) {
+
+	function finishPredict(predictInstance, cb) {
+		predictInstance.estimates({'status': estimateStatusConfig.open}, function(err, estimateList) {
+			if (err)
+				return cb(err)
+			var time = utility.getUnixTimeStamp()
+			for (var i = 0; i < estimateList.length; i++) {
+				var status = estimateStatusConfig.lose
+				if (ctx.args.data.occurrence == 1)
+					status = estimateStatusConfig.win
+				estimateList[i].updateAttributes({'status': status, 'checkTime': time}, function(err, updateInstance) {
+					if (err)
+						return cb(err)
+					if (ctx.args.data.occurrence == 1) {
+						updateInstance.client(function(err, clientInst) {
+							if (err)
+								return cb(err)
+							var newRoundWins = clientInst.accountInfoModel.roundWins + 1
+							var newTotalPoints = client.accountInfoModel.totalPoints + predictInstance.point
+							clientInst.accountInfo.update({'roundWins': newRoundWins, 'totalPoints': newTotalPoints}, function(err, accountInst) {
+								if (err)
+									return cb(err)
+								var ranking = app.models.ranking
+								ranking.find({'where':{'clientId': clientInst.id}}, function(err, rankingList) {
+									if (err)
+										return cb(err)
+									for (var j = 0; j < rankingList.length; j++) {
+										var innerPoints = rankingList[i].points + predictInstance.point
+										rankingList[i].updateAttribute('points', innerPoints, function(err, res) {
+											if (err)
+												return cb(err)
+											if (i == estimateList.length)
+												return cb(null)											
+										})
+									}
+								})
+							})
+						})
+					}
+					else {
+						return cb(null)
+					}
+				})
+			}					
+		})
+	}
+
+	var startPredicts = cron.job("0 */1 * * * *", function () {
+		var time = utility.getUnixTimeStamp()
+		predict.find({
+			where: {
+				'status': statusConfig.created
+			}
+		}, function (err, predictList) {
+			if (err)
+				console.error(err)
+			for (var i = 0; i < predictList.length; i++) {
+				if (predictList[i].beginningTime <= time && predictList[i].endingTime >= time) {
+					predictList[i].updateAttribute('status', statusConfig.working, function (err, predictInst) {
+						if (err)
+							console.error(err)
+					})
+				}
+			}
+		})
+	})
+
+	var finishPredicts = cron.job("0 */1 * * * *", function () {
+		var time = utility.getUnixTimeStamp()
+		predict.find({
+			where: {
+				'status': statusConfig.working
+			}
+		}, function (err, predictList) {
+			if (err)
+				console.error(err)
+			for (var i = 0; i < predictList.length; i++) {
+				if (predictList[i].endingTime <= time) {
+					predictList[i].updateAttribute('status', statusConfig.finished, function (err, predictInst) {
+						if (err)
+							console.error(err)
+						finishPredict(predictInst, function(err, result) {
+							if (err)
+								console.error(err)
+						})
+					})
+				}
+			}
+		})
+	})
+
+	startPackages.start()
+	finishPackages.start()
 
   predict.afterRemote('create', function (ctx, modelInstance, next) {
 		var league = app.models.league
@@ -19,39 +113,13 @@ module.exports = function(predict) {
   predict.afterRemote('replaceById', function (ctx, modelInstance, next) {
 		if (ctx.args.data.status === statusConfig.working && (ctx.args.data.occurrence && ctx.args.data.occurrence != 0)) {
 			var client = app.models.client
-			modelInstance.updateAttribute('status', statusConfig.finished, function(err, instance) {
+			modelInstance.updateAttribute('status', statusConfig.finished, function(err, predictInstance) {
 				if (err)
 					return next(err)
-				instance.estimates({'status': estimateStatusConfig.open}, function(err, estimateList) {
+				finishPredict(predictInstance, function(err, result) {
 					if (err)
 						return next(err)
-					var time = utility.getUnixTimeStamp()
-					for (var i = 0; i < estimateList.length; i++) {
-						var status = estimateStatusConfig.lose
-						if (ctx.args.data.occurrence == 1)
-							status = estimateStatusConfig.win
-						estimateList[i].updateAttributes({'status': status, 'checkTime': time}, function(err, updateInstance) {
-							if (err)
-								return next(err)
-							if (ctx.args.data.occurrence == 1) {
-								updateInstance.client(function(err, clientInst) {
-									if (err)
-										return next(err)
-									var newRoundWins = clientInst.accountInfoModel.roundWins + 1
-									var newTotalPoints = client.accountInfoModel.totalPoints + modelInstance.point
-									clientInst.accountInfo.update({'roundWins': newRoundWins, 'totalPoints': newTotalPoints}, function(err, accountInst) {
-										if (err)
-											return next(err)
-										if (i == estimateList.length)
-											return next()
-									})
-								})
-							}
-							else {
-								return next()
-							}
-						})
-					}					
+					return next()
 				})
 			})
 		}
