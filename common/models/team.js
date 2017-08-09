f9ddabc25b9301ee2ac3d274de36dfa913af8784
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-08-09 09:53:32 +0430

    Make a Copy of Team Name in Client Model

--- common/models/team.js
+++ common/models/team.js
@@ -1,36 +1,46 @@
 var app = require('../../server/server')
 
-module.exports = function(team) {
+module.exports = function (team) {
 
-  team.validatesUniquenessOf('code', {message: 'code is not unique'});
+  team.validatesUniquenessOf('code', {
+    message: 'code is not unique'
+  });
 
   team.selectFavorite = function (ctx, clientId, teamId, cb) {
-		var client = app.models.client
-		client.findById(clientId, function(err, clientInst) {
-			if (err)
-				return cb(err)
-			if (ctx.req.accessToken.userId !== clientId)
-				return cb(new Error('Owner Error'))
-			team.findById(teamId, function(err, teamInst) {
-				if (err)
-					return cb(err)
-				teamInst.clients.add(clientInst, function(err, result) {
-					if (err)
-						return cb(err)
-					return cb(result)
-				})
-			})
-		})
+    var client = app.models.client
+    client.findById(clientId, function (err, clientInst) {
+      if (err)
+        return cb(err)
+      if (ctx.req.accessToken.userId !== clientId)
+        return cb(new Error('Owner Error'))
+      team.findById(teamId, function (err, teamInst) {
+        if (err)
+          return cb(err)
+        teamInst.clients.add(clientInst, function (err, result) {
+          if (err)
+            return cb(err)
+          var model = {
+            teamId: teamId,
+            name: teamInst.name
+          }
+          clientInst.updateAttribute('favTeam', model, function (err, result) {
+            if (err)
+              return cb(err)
+            return cb(result)
+          })
+        })
+      })
+    })
   }
 
   team.remoteMethod('selectFavorite', {
     accepts: [{
-			arg: 'ctx',
-			type: 'object',
-			http: {
-				source: 'context'
-			}
-		}, {
+      arg: 'ctx',
+      type: 'object',
+      http: {
+        source: 'context'
+      }
+    }, {
       arg: 'clientId',
       type: 'string',
       http: {
