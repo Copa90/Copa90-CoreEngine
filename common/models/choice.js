be258bff91bf83b913eab054be3caa4ced83a7b3
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-08-31 18:39:53 +0430

    Fix Choice Algorithm for Once Choice and Fix Typo

--- common/models/choice.js
+++ common/models/choice.js
@@ -46,22 +46,42 @@ module.exports = function(choice) {
 								return next (new Error('خطا! شما حتما باید اولین انتخاب خود را پر کنید'))
 							if (ctx.args.data.firstOption && !ctx.args.data.secondOption && ctx.args.data.thirdOption)
 								return next (new Error('خطا! شما حتما باید دومین انتخاب خود را پر کنید'))
+							if (ctx.args.data.firstOption.choice) {
+								if (ctx.args.data.secondOption.choice) {
+									if (ctx.args.data.firstOption.choice === ctx.args.data.secondOption.choice)
+										return next (new Error('خطا! انتخاب اول و دوم شما نباید یکسان باشد'))
+									if (ctx.args.data.thirdOption.choice) {
+										if (ctx.args.data.thirdOption.choice === ctx.args.data.secondOption.choice)
+											return next (new Error('خطا! انتخاب دوم و سوم شما نباید یکسان باشد'))						
+									}
+								}
+								if (ctx.args.data.thirdOption.choice) {
+									if (ctx.args.data.firstOption.choice === ctx.args.data.thirdOption.choice)
+										return next (new Error('انتخاب اول و سوم شما نباید یکسان باشد'))					
+								}
+							}
 							ctx.args.data.status = statusConfig.open
 							ctx.args.data.topic = exactInst.topic
 							ctx.args.data.leagueName = leagueInst.name
 							for (var i = 0; i < exactInst.selectors.length; i++) {
 								var model = exactInst.selectors[i]
-								if (ctx.args.data.firstOption.choice && model["priority"] === choicePriorityConfig.high) {
-									ctx.args.data.firstOption.point = Number(model.point)
-									ctx.args.data.firstOption.status = statusConfig.open
+								if (ctx.args.data.firstOption) {
+									if (ctx.args.data.firstOption.choice === model.choice) {
+										ctx.args.data.firstOption.point = Number(model.point.first)
+										ctx.args.data.firstOption.status = statusConfig.open
+									}	
 								}
-								if (ctx.args.data.secondOption.choice && model["priority"] === choicePriorityConfig.average) {
-									ctx.args.data.secondOption.point = Number(model.point)
-									ctx.args.data.secondOption.status = statusConfig.open
+								if (ctx.args.data.secondOption) {
+									if (ctx.args.data.secondOption.choice === model.choice) {
+										ctx.args.data.secondOption.point = Number(model.point.second)
+										ctx.args.data.secondOption.status = statusConfig.open
+									}
 								}
-								if (ctx.args.data.thirdOption.choice && model["priority"] === choicePriorityConfig.low) {
-									ctx.args.data.thirdOption.point = Number(model.point)
-									ctx.args.data.thirdOption.status = statusConfig.open
+								if (ctx.args.data.thirdOption) {
+									if (ctx.args.data.thirdOption.choice === model.choice) {
+										ctx.args.data.thirdOption.point = Number(model.point.third)
+										ctx.args.data.thirdOption.status = statusConfig.open
+									}
 								}
 							}			
 							return next()		
@@ -95,45 +115,71 @@ module.exports = function(choice) {
 					if (choiceInst.firstOption.choice && choiceInst.secondOption.choice && choiceInst.thirdOption.choice)
 						return next(new Error('خطا! شما انتخاب دیگری نمیتوانید برای این پیش‌بینی دقیق انجام دهید'))
 
+					if (ctx.args.data.firstOption.choice) {
+						if (ctx.args.data.secondOption.choice) {
+							if (ctx.args.data.firstOption.choice === ctx.args.data.secondOption.choice)
+								return next (new Error('خطا! انتخاب اول و دوم شما نباید یکسان باشد'))
+							if (ctx.args.data.thirdOption.choice) {
+								if (ctx.args.data.thirdOption.choice === ctx.args.data.secondOption.choice)
+									return next (new Error('خطا! انتخاب دوم و سوم شما نباید یکسان باشد'))						
+							}
+						}
+						if (ctx.args.data.thirdOption.choice) {
+							if (ctx.args.data.firstOption.choice === ctx.args.data.thirdOption.choice)
+								return next (new Error('انتخاب اول و سوم شما نباید یکسان باشد'))					
+						}
+					}
+
 					var totalCount = 0
 					if (choiceInst.firstOption.choice) {
-						if (choiceInst.firstOption.choice !== ctx.args.firstOption.choice)
+						if (choiceInst.firstOption.choice !== ctx.args.data.firstOption.choice)
 							return next(new Error('خطا! شما نمیتوانید انتخاب اول خود را تغییر دهید'))
+						else {
+							ctx.args.data.firstOption.byEdit = false
+							ctx.args.data.firstOption.point = choiceInst.firstOption.point
+							ctx.args.data.firstOption.status = choiceInst.firstOption.status
+						}
 					}
 					if (choiceInst.secondOption.choice) {
-						if (choiceInst.secondOption.choice !== ctx.args.secondOption.choice)
+						if (choiceInst.secondOption.choice !== ctx.args.data.secondOption.choice)
 							return next(new Error('خطا! شما نمیتوانید انتخاب دوم خود را تغییر دهید'))
-						else
-							ctx.argd.data.secondOption.byEdit = false
+						else {
+							ctx.args.data.secondOption.byEdit = false
+							ctx.args.data.secondOption.point = choiceInst.secondOption.point
+							ctx.args.data.secondOption.status = choiceInst.secondOption.status
+						}
 					}
 					else {
-						if (ctx.args.secondOption.choice) {
+						if (ctx.args.data.secondOption.choice) {
 							totalCount++							
 							for (var i = 0; i < exactInst.selectors.length; i++) {
 								var model = exactInst.selectors[i]
-								if (ctx.args.data.secondOption.choice && model["priority"] === choicePriorityConfig.average) {
-									ctx.args.data.secondOption.point = Number(model.point)
+								if (ctx.args.data.secondOption.choice === model.choice) {
+									ctx.args.data.secondOption.point = Number(model.point.second)
 									ctx.args.data.secondOption.status = statusConfig.open
-									ctx.argd.data.secondOption.byEdit = true
+									ctx.args.data.secondOption.byEdit = true
 								}
 							}			
 						}
 					}
 					if (choiceInst.thirdOption.choice) {
-						if (choiceInst.thirdOption.choice !== ctx.args.thirdOption.choice)
+						if (choiceInst.thirdOption.choice !== ctx.args.data.thirdOption.choice)
 							return next(new Error('خطا! شما نمیتوانید انتخاب سوم خود را تغییر دهید'))
-						else 
-							ctx.argd.data.thirdOption.byEdit = false
+						else {
+							ctx.args.data.thirdOption.byEdit = false
+							ctx.args.data.thirdOption.point = choiceInst.thirdOption.point
+							ctx.args.data.thirdOption.status = choiceInst.thirdOption.status
+						}
 					}
 					else {
-						if (ctx.args.thirdOption.choice) {
+						if (ctx.args.data.thirdOption.choice) {
 							totalCount++							
 							for (var i = 0; i < exactInst.selectors.length; i++) {
 								var model = exactInst.selectors[i]
-								if (ctx.args.data.thirdOption.choice && model["priority"] === choicePriorityConfig.low) {
-									ctx.args.data.thirdOption.point = Number(model.point)
+								if (ctx.args.data.thirdOption.choice === model.choice) {
+									ctx.args.data.thirdOption.point = Number(model.point.third)
 									ctx.args.data.thirdOption.status = statusConfig.open
-									ctx.argd.data.thirdOption.byEdit = true
+									ctx.args.data.thirdOption.byEdit = true
 								}
 							}			
 						}
@@ -143,6 +189,10 @@ module.exports = function(choice) {
 						return next(new Error('خطا! فرصت‌های شما برای پیش‌بینی تمام شده‌است'))
 					if (Number(clientInst.accountInfoModel.chances) < totalCount)
 						return next(new Error('خطا! فرصت‌های شما برای پیش‌بینی کافی‌ نیست'))			
+
+					ctx.args.data.status = choiceInst.status
+					ctx.args.data.topic = choiceInst.topic
+					ctx.args.data.leagueName = choiceInst.leagueName
 
 					return next()
 				})
@@ -191,9 +241,9 @@ module.exports = function(choice) {
 				var totalCount = 0
 				if (modelInstance.firstOption.choice && modelInstance.firstOption.byEdit)
 					totalCount++
-				if (modelInstance.secondOption.choice && modelInstance.firstOption.byEdit)
+				if (modelInstance.secondOption.choice && modelInstance.secondOption.byEdit)
 					totalCount++
-				if (modelInstance.thirdOption.choice && modelInstance.firstOption.byEdit)
+				if (modelInstance.thirdOption.choice && modelInstance.thirdOption.byEdit)
 					totalCount++
 				var newChances = Number(clientInst.accountInfoModel.chances) - totalCount
 				var newTotalChoices = Number(clientInst.accountInfoModel.totalChoices) + totalCount
