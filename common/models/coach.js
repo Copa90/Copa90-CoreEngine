86058f071f2ea1224e356f1438f7924789f9400a
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-09-03 17:43:59 +0430

    Remove Create Before Remoot Hook of Coach Model

--- common/models/coach.js
+++ common/models/coach.js
@@ -1,26 +1,3 @@
 module.exports = function(coach) {
 
-	coach.beforeRemote('create', function (ctx, modelInstance, next) {
-		if(Object.prototype.toString.call(ctx.args.data) === '[object Array]') {
-			var counter = 0
-			for (var i = 0; i < ctx.args.data.length; i++) {
-				var model = ctx.args.data[i]
-				if (!model.teamId)
-					return next()
-				var name = model.name + ' - ' + model.team
-				model.name = name
-				counter++
-				if (counter == ctx.args.data.length)
-					return next()
-			}
-		}
-		else {
-			if (!ctx.args.data.teamId)
-				return next()
-			var name = ctx.args.data.team + ' - ' + ctx.args.data.name
-			ctx.args.data.name = name
-			return next()				
-		}
-	})
-
 }
