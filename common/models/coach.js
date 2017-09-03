404946c0877d88492a8536d2fd376136b541087c
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-09-03 20:15:31 +0430

    Implement Algorithm to Remove Team Name from Coach Name

--- common/models/coach.js
+++ common/models/coach.js
@@ -1,3 +1,23 @@
 module.exports = function(coach) {
 
+	coach.find(function(err, coachList) {
+		if (err)
+			return console.error(err)
+		for (var i = 0; i < coachList.length; i++) {
+			var model = coachList[i]
+			if (model.name.includes('-')) {
+				var res = model.name.split(" - ")
+				var newName = res[0]
+				console.log(newName)
+				// model.updateAttribute('name', newName, function (err) {
+				// 	if (err)
+				// 		console.error(err)
+				// })
+			}
+			else {
+				continue
+			}
+		}
+	})
+
 }
