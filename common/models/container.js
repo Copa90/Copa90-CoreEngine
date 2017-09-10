84d45011c9585d32407c6a944bccb99ff34c907d
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-09-10 19:22:19 +0430

    Implement Regenraate Sample Profile Image Remote Method

--- common/models/container.js
+++ common/models/container.js
@@ -16,6 +16,8 @@ module.exports = function(container) {
 		client.findById(clientId.toString(), function(err, clientInst) {
 			if (err)
 				return cb(err)
+      if (!clientInst)
+        return cb(new Error('خطا! کاربری با این مشخصات وجود ندارد'))
 			clientInst.updateAttribute('profilePath', profilePath, function(err, result) {
 				if (err)
 					return cb(err)
@@ -54,6 +56,49 @@ module.exports = function(container) {
     description: 'upload a sample image with jdenticon',
     http: {
       path: '/uploadSampleProImage/:clientId',
+      verb: 'POST',
+      status: 200,
+      errorStatus: 400
+    },
+    returns: {
+      type: 'string',
+      root: true
+    }
+  })
+
+	container.regenrateSampleProImage = function(cb) {
+    var client = app.models.client
+    client.find({where:{phoneNumber:{neq: '09120001122'}}}, function(err, clientList) {
+      if (err)
+        return cb(err)
+      var counter = 0
+      for (var i = 0; i < clientList.length; i++) {
+        var model = clientList[i]
+        var directory = path.resolve(__dirname + '/../../fileStorage/')
+        var fp = directory + '/' + model.id.toString() + '/profile.png'
+        var option = {}
+        option.name = '' + model.id.toString()
+        container.createContainer(option, function (err, res) {
+          if (err)
+            return cb(err)
+          console.log(JSON.stringify(res))
+          container.uploadSampleProImage(res.name.toString(), function(err, result) {
+            if (err)
+              return cb(err)
+            counter++
+            if (counter == clientList.length)
+              return cb(null, 'successfully regenerated')
+          })
+        })          
+      }
+    })
+	}
+
+  container.remoteMethod('regenrateSampleProImage', {
+    accepts: [],
+    description: 'regenrate a sample image with jdenticon',
+    http: {
+      path: '/regenrateSampleProImage',
       verb: 'POST',
       status: 200,
       errorStatus: 400
